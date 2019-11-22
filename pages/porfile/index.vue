8f68393a03cc8d5d262959c5d62368bb0fca23a1
SaiDeng <250242742@qq.com>
2019-11-22 22:17:45 +0800

    框架完成

---
+++ pages/porfile/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="">Profile</view>
+</template>
+
+<script>
+	export default{
+		name:'profile',
+		data() {
+			return {
+				key: ""
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
