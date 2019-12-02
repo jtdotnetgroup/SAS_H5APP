6e2f348e2624318f4a5c7ca0362587b711650de8
Vigny <944319975@qq.com>
2019-12-02 09:31:01 +0800

    提交
    提交

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App'
+<<<<<<< Updated upstream
 
+=======
+import store from './store/index.js'
+import moment from 'moment'
+>>>>>>> Stashed changes
 
 Vue.config.productionTip = false
+
+moment.locale('cn')
+Vue.prototype.$moment = moment
 
 App.mpType = 'app'
 
