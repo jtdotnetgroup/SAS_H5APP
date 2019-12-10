298dc60d683702393a550566b87656f064bc3633
Sximsing <3010069635@qq.com>
2019-12-10 14:33:56 +0800

    格式
    格式

--- components/swiper-item/swiper-item.vue
+++ components/swiper-item/swiper-item.vue
@@ -6,16 +6,16 @@
 					<view class="sameLine">
 						<view class="uni-steps__column-title" @click="toRepair(todo.id,ticketId)">
 							{{todo.title}}
-							<span class="iconfont iconnaozhong alarmClock" ></span>
+							<span class="iconfont iconnaozhong alarmClock"></span>
 							<span class="descstyle">{{dateTime(todo.desc)}}</span>
 						</view>
 						<view class="uni-steps__column-desc">
-							计划时间：{{ todo.desc }}	
+							计划时间：{{ todo.desc }}
 						</view>
 					</view>
 					<view class="sameLine fiexRight">
 						<span v-if="todo.status === 1">
-							<span class="iconfont iconchenggong iconSuccess" ></span>
+							<span class="iconfont iconchenggong iconSuccess"></span>
 						</span>
 						<span v-else>
 							<span class="iconfont icontanhao exclamationMark "></span>
@@ -28,14 +28,16 @@
 </template>
 
 <script>
-	import {calculationTime} from '@/utils/moment.js'
+	import {
+		calculationTime
+	} from '@/utils/moment.js'
 
 	export default {
 		components: {
 			stepDevice: () => import('@/components/step-device/step-device.vue')
 		},
-		props:{
-			ticketId:{
+		props: {
+			ticketId: {
 				type: String,
 				default () {
 					return ""
@@ -50,12 +52,12 @@
 					title: '完工汇报',
 					desc: '2019-05-11 12:36:44',
 					status: 0
-				},{
+				}, {
 					id: '5',
 					title: '维修工单',
 					desc: '2019-06-04 15:45:11',
 					status: 0
-				},{
+				}, {
 					id: '4',
 					title: '故障处理',
 					desc: '2019-07-01 10:11:35',
@@ -71,15 +73,15 @@
 					this.active = 0
 				}
 			},
-			toRepair(stageId,ticketId) {
+			toRepair(stageId, ticketId) {
 				uni.navigateTo({
-					url: '../../mytask/repair/workOrderRepair?id='+ stageId + "&ticketId="+ticketId
+					url: '../../mytask/repair/workOrderRepair?id=' + stageId + "&ticketId=" + ticketId
 				})
 			}
 		},
 		computed: {
-			dateTime(time){
-				return time =>{
+			dateTime(time) {
+				return time => {
 					return calculationTime(time);
 				}
 			}
@@ -202,39 +204,39 @@
 		padding: 15px;
 		flex-direction: row;
 	}
-	
+
 
 	/* 自定义 */
-	.alarmClock{
-		padding-left:110rpx;
-		padding-right:15rpx;
-		color:#4395ff;
+	.alarmClock {
+		padding-left: 110rpx;
+		padding-right: 15rpx;
+		color: #4395ff;
 	}
-	
-	.iconSuccess{
-		font-weight:bold;
+
+	.iconSuccess {
+		font-weight: bold;
 		font-size: 21px;
-		color:rgb(82, 196, 26);
+		color: rgb(82, 196, 26);
 	}
-	
-	.exclamationMark{
-		font-weight:600;
+
+	.exclamationMark {
+		font-weight: 600;
 		font-size: 18px;
 		color: rgb(208, 2, 27);
 	}
-	
-	.iconstyle{
+
+	.iconstyle {
 		font-size: 14px;
 		color: #DD524D;
 	}
-	
+
 	.sameLine {
 		display: inline-block;
 	}
-	
+
 	.fiexRight {
-		float:right;
+		float: right;
 		/* position: absolute; */
-		margin:10px;
+		margin: 10px;
 	}
 </style>
