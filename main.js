333af03c2559318951c9e3344193ee03e1deffe8
Vigny <944319975@qq.com>
2020-01-13 14:09:24 +0800

    更新
    更新

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ Vue.prototype.$url = 'http://120.77.40.245:8691/'
 // Vue.prototype.$agentId = '346116753'
 // Vue.prototype.$url = 'http://40.73.75.101:8090/'	
 
-//moment.locale('cn')
-//Vue.prototype.$moment = moment
-//Vue.prototype.$IP = 'http://120.77.40.245:8080/sac'//捷特
+moment.locale('cn')
+Vue.prototype.$moment = moment
+Vue.prototype.$IP = 'http://120.77.40.245:8080/sac'//捷特
 // Vue.prototype.$IP = 'http://40.73.75.101:8080'// 索奥斯
 
 App.mpType = 'app'
