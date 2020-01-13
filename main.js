7966f1139f9b8ba3675f0c9744dce2ea1cd5ea01
lishenwen <3010069635@qq.com>
2020-01-13 18:58:29 +0800

    main
    main

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ Vue.prototype.$url = 'http://120.77.40.245:8691/'
 
 moment.locale('cn')
 Vue.prototype.$moment = moment
-//Vue.prototype.$IP = 'http://120.77.40.245:8080/sac'//捷特
-//Vue.prototype.$IP = 'http://40.73.75.101:8080'// 索奥斯
-Vue.prototype.$IP = 'http://192.168.3.8:8096'// 
+// Vue.prototype.$IP = 'http://120.77.40.245:8080/sac'//捷特
+Vue.prototype.$IP = 'http://40.73.75.101:8080'// 索奥斯
+// Vue.prototype.$IP = 'http://192.168.3.8:8096'// 
 
 App.mpType = 'app'
 
