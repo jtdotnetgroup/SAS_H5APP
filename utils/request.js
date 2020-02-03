0126f2bbcee39fdada22becb6544b81eecbe095b
lishenwen <3010069635@qq.com>
2020-02-03 19:18:38 +0800

    ip
    ip

--- utils/request.js
+++ utils/request.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const http = axios.create({
-	// baseURL: 'http://192.168.3.8:8096',// 本地
-	//baseURL: 'http://120.77.40.245:8080/sac',// 捷特
-	baseURL: 'http://40.73.75.101:8080',// 索奥斯
+	baseURL: 'http://192.168.2.102:8096',// 本地
+	// baseURL: 'http://120.77.40.245:8080/sac',// 捷特
+	// baseURL: 'http://40.73.75.101:8080',// 索奥斯
 	timeout: 50000
 })
 
@@ -18,7 +18,7 @@ http.interceptors.request.use(function (config) {
 	if (config.url.indexOf('getAccessToken') < 0) {
 		config.headers.common['userId'] = sessionStorage.getItem('userId')
 		config.headers.common['token'] = sessionStorage.getItem('token')
-		config.headers.common['unVerify'] = '0'
+		config.headers.common['unVerify'] = '1'
 	}
     return config;
   }, function (error) {
